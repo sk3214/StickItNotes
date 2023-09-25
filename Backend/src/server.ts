86d4ee31eb8a6987c9db190b5f5bf0ecf84c7016
unknown <sagarkhuteta26@gmail.com>
2023-09-25 14:36:29 -0400

    some cleanup

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -19,7 +19,7 @@ app.use(session({
     resave:false,
     saveUninitialized:false,
     cookie:{
-        maxAge:60*60*1000
+        maxAge:60*60*1000,
     },
     rolling:true,
     store:MongoStore.create({
